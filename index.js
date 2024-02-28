613cf2de8efe47149d871f3dc5744b12ace1ab96
Gabriela Dyworova <gabriela.dyworova@gmail.com>
2024-02-28 08:29:46 +0100

    oprava

---
+++ index.js
@@ -0,0 +1,12 @@
+const vysledek = document.querySelector = ".vysledek"
+
+const padlOrel = Math.random() < 0.5;
+
+if (padlOrel === true) {
+  "Padl Orel" 
+  
+} else {
+  "Padla Pana"
+}
+
+document.innerHTML= padlOrel;
